990fdb10a4e8c81a0e55c21dfb6f65d5e6f028d2
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-07-29 22:57:57 +0500

    add

--- test/Lock.ts
+++ test/Lock.ts
@@ -4,9 +4,6 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshopt in every test.
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1_000_000_000;
@@ -14,7 +11,6 @@ describe("Lock", function () {
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Lock = await ethers.getContractFactory("Lock");
@@ -47,7 +43,6 @@ describe("Lock", function () {
     });
 
     it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
       const latestTime = await time.latest();
       const Lock = await ethers.getContractFactory("Lock");
       await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
@@ -71,10 +66,8 @@ describe("Lock", function () {
           deployOneYearLockFixture
         );
 
-        // We can increase the time in Hardhat Network
         await time.increaseTo(unlockTime);
 
-        // We use lock.connect() to send a transaction from another account
         await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
           "You aren't the owner"
         );
